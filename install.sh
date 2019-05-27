0cb6fc05a87d115d3aef95a5c2058fbdb2f90648
San-ny <inventor561@gmail.com>
2019-05-27 10:43:27 +0200

    ~install scrpt ++ precompiled jar
    Signed-off-by: San-ny <inventor561@gmail.com>

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Downloading latest transferTool sourcecode from git repositories\n'
+
+sudo mkdir /tmp/transfertool
+cd /tmp/transfertool
+
+sudo git clone https://github.com/San-Ny/TransferTool
+
+echo 'compilating with java\n'
+
+
